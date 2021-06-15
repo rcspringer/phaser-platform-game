c1dd9c452267cb60e3bc68e075c07ff64e9e2882
R Springer <rspringer@rocmondriaan.nl>
2021-06-15 18:08:13 +0200

    Centered the Game Over text

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -7,10 +7,12 @@ export default class GameOver extends Phaser.Scene {
 
   create() {
     const {width, height} = this.scale;
-    this.add.text(width / 2, height / 2, 'Game Over', {
+    const text = this.add.text(width / 2, height / 2, 'Game Over', {
       fontSize: '30px',
       align: 'center',
     });
+    text.setX(width * 0.5 - text.width * 0.5);
+    text.setY(height * 0.5 - text.height * 0.5 - 24);
     this.input.keyboard.on('keyup', this.resetGame, this);
     // Removing the event COIN_COLLECTED when this scene is destroyed
     this.events.once('destroy', () => {

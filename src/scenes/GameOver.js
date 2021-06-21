1d72ff7000a6189ce52057b3c1d519d3ce7289f5
Roy Springer <rspringer@slip-app.nl>
2021-06-21 13:52:03 +0200

    Added Restart to the game

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -7,25 +7,22 @@ export default class GameOver extends Phaser.Scene {
 
     create() {
         const { width, height } = this.scale;
-        const text = this.add.text(width / 2, height / 2, 'Game Over', {
+        const text = this.add.text(width * 0.5, height * 0.4, 'Game Over', {
             fontSize: '30px',
-            align: 'center',
         });
-        text.setX(width * 0.5 - text.width * 0.5);
-        text.setY(height * 0.5 - text.height * 0.5 - 24);
+        text.setOrigin(0.5);
         this.input.keyboard.on('keyup', this.resetGame, this);
-        // Removing the event COIN_COLLECTED when this scene is destroyed
+        // Removing the event when this scene is destroyed
         this.events.once('destroy', () => {
             this.input.keyboard.off('keyup', this.resetGame, this);
-            // Relaunch the game
-            this.scene.restart('game');
         });
     }
 
     resetGame(event) {
         const code = event.keyCode;
         if (code === Phaser.Input.Keyboard.KeyCodes.R) {
-            this.scene.remove('game-over');
+            // Relaunch the game
+            this.scene.start('game');
         }
     }
 }
